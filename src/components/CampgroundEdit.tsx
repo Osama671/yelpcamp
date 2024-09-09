cb333a69b64997dd1dd8bd5123f13888bb4ff2fa
Osama9991 <osamaazar48@hotmail.com>
2024-09-09 12:58:19 -0400

    Validation for editing campgrounds

--- src/components/CampgroundEdit.tsx
+++ src/components/CampgroundEdit.tsx
@@ -1,10 +1,63 @@
 import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
 import axios from "axios";
+
+const validate = (values) => {
+  const errors = {};
+  if (!values.title) {
+    errors.title = "Required";
+  } else if (values.title.length >= 10) {
+    errors.title = "Must be less than 10 characters";
+  }
+
+  if (!values.location) {
+    errors.location = "Required";
+  } else if (values.location.length >= 10) {
+    errors.location = "Must be less than 10 characters";
+  }
+
+  if (!values.price) {
+    errors.price = "Required";
+  } else if (values.price <= 0) {
+    errors.price = "Must be greater than 0";
+  }
+
+  if (!values.imageurl) {
+    errors.imageurl = "Required";
+  }
+
+  if (!values.description) {
+    errors.description = "Required";
+  } else if (values.description <= 5) {
+    errors.description = "Be a bit more descriptive!";
+  }
+  return errors;
+};
 
 export default function CampgroundEdit() {
   const [campground, setCampground] = useState([]);
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      location: "",
+      price: "",
+      imageurl: "",
+      description: "",
+    },
+    validate,
+    onSubmit: async (values) => {
+      // alert(JSON.stringify(values, null, 2));
+      const response = await axios.post(`/api/campgrounds/${id}/edit`, values);
+      console.log(response.status);
+      if (response.status === 200) {
+        navigate(`/campground/${id}`);
+      }
+    },
+  });
 
   useEffect(() => {
     async function getAPI() {
@@ -18,7 +71,7 @@ export default function CampgroundEdit() {
       <h1 className="text-center">Edit Campground:</h1>
       <p className="text-center"></p>
       <div className="col-6 offset-3">
-        <form action={`/api/campgrounds/${id}/edit`} method="POST">
+        <form onSubmit={formik.handleSubmit}>
           <div className="mb-3">
             <label className="form-label" htmlFor="title">
               Title
@@ -29,7 +82,13 @@ export default function CampgroundEdit() {
               type="text"
               name="title"
               placeholder={campground.title}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.title}
             />
+            {formik.touched.title && formik.errors.title ? (
+              <div style={{ color: "red" }}>{formik.errors.title}</div>
+            ) : null}
           </div>
           <div className="mb-3">
             <label className="form-label" htmlFor="location">
@@ -41,9 +100,15 @@ export default function CampgroundEdit() {
               type="text"
               name="location"
               placeholder={campground.location}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.location}
             />
+            {formik.touched.location && formik.errors.location ? (
+              <div style={{ color: "red" }}>{formik.errors.location}</div>
+            ) : null}
           </div>
-          <div className="input-group mb-3">
+          <div className="input-group">
             <span className="input-group-text">$</span>
             <input
               type="text"
@@ -52,9 +117,17 @@ export default function CampgroundEdit() {
               id="price"
               name="price"
               placeholder={campground.price}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.price}
             />
           </div>
-          <div className="mb-3">
+          {formik.touched.price && formik.errors.price ? (
+            <div style={{ color: "red", marginBottom: "1em" }}>
+              {formik.errors.price}
+            </div>
+          ) : null}
+          <div className="mb- mt-3">
             <label className="form-label" htmlFor="imageurl">
               Image Url
             </label>
@@ -64,7 +137,13 @@ export default function CampgroundEdit() {
               type="text"
               name="imageurl"
               placeholder={campground.image}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.imageurl}
             />
+            {formik.touched.price && formik.errors.price ? (
+              <div style={{ color: "red" }}>{formik.errors.price}</div>
+            ) : null}
           </div>
           <div className="mb-3">
             <label className="form-label" htmlFor="description">
@@ -75,7 +154,13 @@ export default function CampgroundEdit() {
               id="description"
               name="description"
               placeholder={campground.description}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.description}
             ></textarea>
+            {formik.touched.description && formik.errors.description ? (
+              <div style={{ color: "red" }}>{formik.errors.description}</div>
+            ) : null}
           </div>
           <button className="btn btn-info" type="submit">
             Update Campground
